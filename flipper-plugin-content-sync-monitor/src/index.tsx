28098d40426917ed744fbd3c0afe22cef96f6224
Jon <jchristofmartin@gmail.com>
2023-08-06 11:46:39 -0600

    add ui and the ability to call client functions

--- flipper-plugin-content-sync-monitor/src/index.tsx
+++ flipper-plugin-content-sync-monitor/src/index.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import {PluginClient, usePlugin, createState, useValue, Layout} from 'flipper-plugin';
-import { Button, Tooltip, Space } from 'antd';
-import { ReloadOutlined } from '@ant-design/icons';
+import { PluginClient, usePlugin, createState, useValue, Layout } from 'flipper-plugin';
+import { Button, Tooltip, Space, DataTable } from 'antd';
+import { ReloadOutlined, ClearOutlined, InfoOutlined } from '@ant-design/icons';
 
 type Data = {
   id: string;
-  event?: string;
-  timestamp
+  type: string;
+  operation: string;
+  status: string;
+  message: string;
+  timestamp: string
 };
 
 type Events = {
-  newData: Data;
+  contentSyncData: Data;
 };
 
 // Read more: https://fbflipper.com/docs/tutorial/js-custom#creating-a-first-plugin
 // API: https://fbflipper.com/docs/extending/flipper-plugin#pluginclient
 export function plugin(client: PluginClient<Events, {}>) {
-  const data = createState<Record<string, Data>>({}, {persist: 'data'});
+  const data = createState<Record<string, Data>>({}, { persist: 'data' });
 
-  client.onMessage('newData', (newData) => {
+  client.onMessage('contentSyncData', (contentSyncData) => {
     data.update((draft) => {
-      draft[newData.id] = newData;
+      draft[contentSyncData.id] = contentSyncData;
     });
   });
 
-  const fullSyncRequest = (params: string) => {
+  const syncRequest = (params: string) => {
     client
-      .send('fullsync', {
+      .send('runSynchronizer', {
         params,
-      })};
+      })
+  };
+
+  const clearTable = ()=>{
+    data.set({});
+  }
 
   client.addMenuEntry({
     action: 'clear',
@@ -40,7 +48,8 @@ export function plugin(client: PluginClient<Events, {}>) {
 
   return {
     data,
-    fullSyncRequest
+    syncRequest,
+    clearTable
   };
 }
 
@@ -48,27 +57,73 @@ export function plugin(client: PluginClient<Events, {}>) {
 // API: https://fbflipper.com/docs/extending/flipper-plugin#react-hooks
 export function Component() {
   const instance = usePlugin(plugin);
-  const data:Data = useValue(instance.data);
+  const data = useValue(instance.data);
 
   return (
     <Layout.ScrollContainer>
       <Space direction="vertical">
-    <Space wrap>
-    <Tooltip title="full sync">
-        <Button 
-        icon={<ReloadOutlined />} 
-        onClick={() => instance.fullSyncRequest("")}
-        />
-      </Tooltip>
-    </Space>
-  </Space>
-      {Object.entries(data).map(([id, d]) => (
-        <pre key={id} data-testid={id}>
-          
-          {console.log(id, d)}
-          {JSON.stringify(d)}
-        </pre>
-      ))}
+        <Space wrap>
+          <Tooltip title="Request app full sync">
+            <Button
+              icon={<ReloadOutlined />}
+              onClick={() => instance.syncRequest("sync_everything")}
+            />
+          </Tooltip>
+          <Tooltip title="Only resync markups">
+            <Button
+              icon={<ReloadOutlined />}
+              onClick={() => instance.syncRequest("resync_markups")}
+            />
+          </Tooltip>
+          <Tooltip title="Clear">
+          <Button
+              icon={<ClearOutlined />}
+              onClick={() => instance.clearTable()}
+            />
+          </Tooltip>
+        </Space>
+      </Space>
+      <table>
+        <thead>
+          <HeaderComponent/>
+        </thead>
+        <tbody>
+          {Object.entries(data).map(([id, d]) => (
+            <DataComponent data={d}></DataComponent>
+          ))}
+        </tbody>
+      </table>
     </Layout.ScrollContainer>
   );
+}
+
+
+function DataComponent({ data: Data }) {
+  return (
+    <tr key={Data.id}>
+      <td>{Data.type}</td>
+      <td>{Data.operation}</td>
+      <td>{Data.status}</td>
+      <td>{Data.timestamp}</td>
+      <td>{Data.message}</td>
+      <td><Button icon={<InfoOutlined />}></Button></td>
+    </tr>
+  )
+}
+
+function HeaderComponent() {
+  const tableHeaderStyle = {
+    textAlign: 'left',
+  };
+
+  return (
+    <tr style={tableHeaderStyle}>
+      <th>Type</th>
+      <th>Operation</th>
+      <th>Status</th>
+      <th>Time</th>
+      <th>Message</th>
+      <th>Inspect</th>
+    </tr>
+  )
 }
